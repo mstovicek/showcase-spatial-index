69bec3f3bdbce41bcefb4a65cd7063c65f2ddfdf
Milan Stovicek <milan.stovicek@rocket-internet.de>
2016-09-20 00:08:02 +0200

    some ui

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express    = require('express'),
     locations  = require('./lib/locations'),
     app = express();
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 /**
  * http://docker.dev:3000/closest/lat/lon/dist
  */
