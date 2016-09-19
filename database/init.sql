6c1d8a86614ce833a7e41b2d23b2b463eea8d11a
Milan Stovicek <milan.stovicek@rocket-internet.de>
2016-09-20 00:28:59 +0200

    auto increment and init fix

--- database/init.sql
+++ database/init.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS `locations` (
   `description` varchar(127) DEFAULT NOT NULL,
   PRIMARY KEY (`id`),
   SPATIAL KEY `geom` (`geom`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- source ./data-mini.sql
 -- source ./data-de.sql
 
-ALTER TABLE `locations` CHANGE `geom` `geom` GEOMETRY NOT NULL;
-
 UPDATE locations SET geom = ST_GeomFromText(CONCAT('POINT(', latitude, ' ', longitude,')'), 0);
+
+ALTER TABLE `locations` CHANGE `geom` `geom` GEOMETRY NOT NULL;
 
 ALTER TABLE locations ADD SPATIAL INDEX(geom);
